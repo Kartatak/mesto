c1da56cb88336c490595e0d6621da0bdb601d7e5
Tatiana Karaseva <tatak@yandex.ru>
2023-01-22 20:22:09 +0400

    update

---
+++ blocks/popup/__submit/popup__submit.js
@@ -0,0 +1,13 @@
+let formElement = document.querySelector(".popup__submit");
+let nameInput = document.querySelector(".popup__name");
+let jobInput = document.querySelector(".popup__job");
+let profileTitle = document.querySelector(".profile__title");
+let profileText = document.querySelector(".profile__text");
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileText.textContent = jobInput.value;
+
+  formElement.addEventListener("submit", handleFormSubmit);
+}
